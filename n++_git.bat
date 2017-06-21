a94228b3b6fa5b5cd20c85df2bdc12f9382106b7
codechaos <hello@codechaos.de>
2017-06-21 15:51:52 +0200

    added script that can start n++ as editor when using git

---
+++ n++_git.bat
@@ -0,0 +1,4 @@
+@echo off
+REM to configure git to use this file use: 
+REM git config --global core.editor n++_git.bat
+"C:\Program Files (x86)\Notepad++\notepad++.exe" -multiInst -notabbar -nosession -noPlugin "%*"
