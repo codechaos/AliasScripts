8dc1e5df4ca6314d75951288624750aa429bbb04
codechaos <me@codechaos.de>
2017-11-05 14:51:34 +0100

    added dev keyboard layout

--- glog.bat
+++ glog.bat
@@ -1,3 +1,2 @@
 @echo off
-REM git log --graph --pretty=format:"%%Cred%%h%%Creset -%%C(yellow)%%d%%Creset %%s %%Cgreen(%%cr) %%C(bold blue)<%%an>%%Creset" --abbrev-commit -n 10 %*
 git log --graph --pretty=format:"%%Cred%%h%%Creset -%%C(yellow)%%d%%Creset %%s %%Cgreen(%%cr) %%C(bold blue)<%%an>%%Creset" --abbrev-commit -n 10 %*
