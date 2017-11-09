93f3b96b6229f21261ffa4994d91fb5ccbdd20d4
codechaos <frank.boehmer@optiz-consulting.de>
2017-11-09 08:25:16 +0100

    added alias for git commit --fixup

---
+++ gfix.bat
@@ -0,0 +1,15 @@
+@echo off
+set showhelp=false
+IF /I "%1" EQU "--help" (set showhelp=true)
+IF /I "%1" EQU "" (set showhelp=true)
+IF [%1]==[] (set showhelp=true)
+
+IF "%showhelp%" == "true" (
+	echo Usage:    gfix ^<COMMIT^>
+	echo Executes: git commit --fixup ^<COMMIT^>
+	echo:
+	echo Use git rebase --autosquach
+	EXIT /B 0
+)
+
+git commit --fixup %1
